da86e8048570ee80a92ed61fc58429fe8cbcae51
kihyun <gosof@naver.com>
2025-05-06 08:32:59 +0900

    Add : 상호작용구역 추가
    - 상호작용구역 추가

--- Metabus/Assets/Scripts/Manager/GameManager.cs
+++ Metabus/Assets/Scripts/Manager/GameManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 // 캐릭터 이동및 탐색
+// 카메라 추적기능
 //===================
 // 맵 설계 및  상호작용
 // 미니게임 실행
 // 점수시스템
 // 게임종료및 복귀
-// 카메라 추적기능
 //==================
 public class GameManager : MonoBehaviour
 {
